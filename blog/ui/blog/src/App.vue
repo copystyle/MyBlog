a00308157ecdaf4f4fd1c4b159ac796f9de2c1f2
徐浩 <xuhao@xuhaodeMacBook-Pro.local>
2018-04-30 16:50:19 +0800

    add vue

--- blog/ui/blog/src/App.vue
+++ blog/ui/blog/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body,div,dl,dt,dd,ul,ol,li,
+h1,h2,h3,h4,h5,h6,pre,code,
+form,fieldset,legend,input,
+textarea,p,blockquote,th,td,button,span {
+  margin: 0;
+  padding: 0;
+}
+ul,li{
+    list-style: none;
 }
 </style>
--- blog/ui/blog/src/main.js
+++ blog/ui/blog/src/main.js
@@ -3,6 +3,54 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-default/index.css'
+import mavonEditor from 'mavon-editor'
+// import 'mavon-editor/dist/css/index.css'
+
+import '@/assets/css/container.css'
+import '@/assets/css/header.css'
+import '@/assets/css/article.css'
+import '@/assets/css/side.css'
+import '@/assets/css/blog.css'
+
+Vue.use(ElementUI)
+Vue.use(mavonEditor)
+
+Vue.prototype.setCookie = (cName, value, expiredays) => {
+  var exdate = new Date()
+  exdate.setDate(exdate.getDate() + expiredays)
+  document.cookie = cName + '=' + escape(value) + ((expiredays == null) ? '' : ';expires=' + exdate.toGMTString())
+}
+
+Vue.prototype.setCookie = (cName, value, expiredays) => {
+  var exdate = new Date()
+  exdate.setDate(exdate.getDate() + expiredays)
+  document.cookie = cName + '=' + escape(value) + ((expiredays == null) ? '' : ';expires=' + exdate.toGMTString())
+}
+/*
+function getCookie (name) {
+  var reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)')
+  if (arr === document.cookie.match(reg)) {
+    return arr[2]
+  } else {
+    return null
+  }
+}
+*/
+function getCookie (name) {
+  return null
+}
+
+Vue.prototype.getCookie = getCookie
+Vue.prototype.delCookie = (name) => {
+  var exp = new Date()
+  exp.setTime(exp.getTime() - 1)
+  var cval = getCookie(name)
+  if (cval != null) {
+    document.cookie = name + '=' + cval + ';expires=' + exp.toGMTString()
+  }
+}
 
 Vue.config.productionTip = false
 
@@ -10,6 +58,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  components: { App }
 })
