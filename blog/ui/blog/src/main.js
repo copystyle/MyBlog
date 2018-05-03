bad80c7753fee8accc3a5f8e74991fa0da76920f
徐浩 <xuhao@xuhaodeMacBook-Pro.local>
2018-05-03 23:49:36 +0800

    merge

--- blog/ui/blog/src/main.js
+++ blog/ui/blog/src/main.js
@@ -3,10 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-default/index.css'
-import mavonEditor from 'mavon-editor'
-// import 'mavon-editor/dist/css/index.css'
+// import ElementUI from '../../node_modules/element-ui'
+// import '../../node_modules/element-ui/lib/theme-chalk/index.css'
+// import mavonEditor from '../../mavon-editor'
+// import '../../node_modules/mavon-editor/dist/css/index.css'
 
 import '@/assets/css/container.css'
 import '@/assets/css/header.css'
@@ -14,8 +14,8 @@ import '@/assets/css/article.css'
 import '@/assets/css/side.css'
 import '@/assets/css/blog.css'
 
-Vue.use(ElementUI)
-Vue.use(mavonEditor)
+// Vue.use(ElementUI)
+// Vue.use(mavonEditor)
 
 Vue.prototype.setCookie = (cName, value, expiredays) => {
   var exdate = new Date()
